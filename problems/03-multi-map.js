20225d4b7dbae25d473ccb20dfd18ad50186e8bd
Kevin Mei <kevjinx@gmail.com>
2021-04-14 13:56:44 -0700

    problem 2-4

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -21,10 +21,28 @@ let result3 = multiMap("hi", 5, function(s) {
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
-let multiMap = function() {
-
+let multiMap = function(el, n, cb) {
+  let result
+  for (let i = 0; i < n; i++) {
+    el = cb(el);
+  }
+  return el;
 };
 
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
+
+let result2 = multiMap(7, 3, function(n) {
+    return n * 10;
+});
+console.log(result2); // 7000
+
+let result3 = multiMap("hi", 5, function(s) {
+  return s + "!";
+});
+console.log(result3); // hi!!!!!
 
 
 
